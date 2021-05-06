feb2aa459cb4acb189f1b62f792403f16749c4a0
Giuseppina <giuseppina.pietroniro@gmail.com>
2021-05-06 09:34:40 +0200

    menu

---
+++ app/src/main/java/Menu.java
@@ -0,0 +1,31 @@
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.evaristo.R;
+
+import java.util.ArrayList;
+
+public class Menu extends AppCompactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        String[] nameproducts = new String[]{"primo", "secondo", "contorno"};
+        final ArrayList <String>listp = new ArrayList<String>();
+        for(int i = 0; i< nameproducts.length; i++){
+            listp.add(nameproducts[i]);
+        }
+        final Menu mylist = (Menu)findViewByld(R.id.listView1);
+
+        final ArrayAdapter <String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1,listp);
+
+        mylist.setAdapter(adapter);
+    }
+
+
+}
