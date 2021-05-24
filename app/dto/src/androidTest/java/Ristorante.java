c09d1d38c986ae4cea39d191a929c4c10f4f12fc
Giuseppina <giuseppina.pietroniro@gmail.com>
2021-05-24 15:34:13 +0200

    DTO classi

--- app/dto/src/androidTest/java/Ristorante.java
+++ app/dto/src/androidTest/java/Ristorante.java
@@ -15,9 +15,55 @@ public class Ristorante extends Activity {
     private String ragione_sociale;
     private String partita_iva;
 
-public Ristorante(){
+public Ristorante(String id, String nome, String indirizzo, String ragione_sociale, String partita_iva){
+
+    this.id = id;
+    this.nome = nome;
+    this.indirizzo = indirizzo;
+    this.ragione_sociale = ragione_sociale;
+    this.partita_iva = partita_iva;
 
 }
+   public String getId(){
+    return id;
+   }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+   public String getNome(){
+    return nome;
+   }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getRagione_sociale() {
+        return ragione_sociale;
+    }
+
+    public void setRagione_sociale(String ragione_sociale) {
+        this.ragione_sociale = ragione_sociale;
+    }
+
+    public String getPartita_iva() {
+        return partita_iva;
+    }
+
+    public void setPartita_iva(String partita_iva) {
+        this.partita_iva = partita_iva;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/dto/src/androidTest/java/Tavolo.java
+++ app/dto/src/androidTest/java/Tavolo.java
@@ -5,4 +5,52 @@ public class Tavolo {
     private String coperti;
     private String descrizione;
     private String stato;
+
+    public Tavolo(String id, String numero, String coperti, String descrizione, String stato){
+        this.id = id;
+        this.numero = numero;
+        this.coperti = coperti;
+        this.descrizione = descrizione;
+        this.stato = stato;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCoperti() {
+        return coperti;
+    }
+
+    public void setCoperti(String coperti) {
+        this.coperti = coperti;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getStato() {
+        return stato;
+    }
+
+    public void setStato(String stato) {
+        this.stato = stato;
+    }
 }
